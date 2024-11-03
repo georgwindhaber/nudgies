3cf3e4b0f1d9e39adefbec8012e4c0b0a31f71ed
Georg <georg.windhaber@gmail.com>
2024-11-03 21:50:08 +0100

    Add google meet addon package

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { meet } from '@googleworkspace/meet-addons/meet.addons'
+const CLOUD_PROJECT_NUMBER = '1028257658812'
+
+const session = await meet.addon.createAddonSession({
+  cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+})
+const sidePanelClient = await session.createSidePanelClient()
+
+console.log(sidePanelClient)
+</script>
+
 <template>
   <main>Nudgies</main>
 </template>
